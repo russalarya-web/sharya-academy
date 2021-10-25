0faf612a82acc09b441b862df2194751af21dc53
Russal Arya <russalarya@gmail.com>
2021-10-25 17:43:38 +0300

    Basic Landing Page Structure

--- sharya-academy/src/App.js
+++ sharya-academy/src/App.js
@@ -1,19 +1,50 @@
-import logo from './logo.svg';
+import triangle from './resources/triangle.png';
+import textLogo from './resources/text-logo.png';
 import './App.css';
 
 import styled, {css} from 'styled-components';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import React, { Component } from "react";
 
+export const Title = styled.h2`
+  color: #1E5128;
+  font-weight: 600;
+  margin: 0.8em 0 0.2em 0;
+  z-index: 1;
+`;
+
+export const Triangle = styled.img`
+  height: 95%;
+  width: 100%;
+  position: fixed;
+  bottom: -10px;
+  z-index: 0;
+`;
+
+export const Screen = styled.div`
+  min-height: 95vh;
+  padding: 2.5vh 20px;
+  width: 100%;
+`;
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Sharya Academy
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Triangle src={triangle} />
+        <div className="AppForeground">
+          <Screen>
+            <img src={textLogo} />
+          </Screen>
+          <Screen>
+            <Title>About</Title>
+          </Screen>
+          <Screen>
+            <Title>Pricing</Title>
+          </Screen>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
