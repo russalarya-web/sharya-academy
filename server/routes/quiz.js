a1068e4557028a81ce8afa408ce776ae7e1b92c7
Russal Arya <russalarya@gmail.com>
2022-01-18 07:36:49 +0300

    Fixed Port Issue on URL

--- server/routes/quiz.js
+++ server/routes/quiz.js
@@ -26,7 +26,7 @@ router.post("/create", function(req, res, next) {
     if (quiz.create(req.body.quizId, req.body.classId, req.body.chapterId, req.body.subjectId, req.body.quizName)) {
         console.log(req.body);
     }
-    res.redirect(currentUrl + ':3000/admin/content');
+    res.redirect(currentUrl + '/admin/content');
 });
 
 // create quiz questions in db
@@ -34,7 +34,7 @@ router.post("/create/question/:quizId", function(req, res, next) {
     if (question.create(req.body.questionId, req.params.quizId, req.body.question, [req.body.option1, req.body.option2, req.body.option3, req.body.option4], req.body.correctOption, req.body.points)) {
         console.log(req.body);
     }
-    res.redirect(currentUrl + ':3000/admin/quiz/' + req.params.quizId);
+    res.redirect(currentUrl + '/admin/quiz/' + req.params.quizId);
 });
 
 
