27ec04994cbc8d8c246df88d681bd1ec8d67b4d3
Russal Arya <russalarya@gmail.com>
2022-01-25 10:19:51 +0300

    Updated CreateQuestion UI

--- client/src/components/adminView/modals/CreateQuestion.js
+++ client/src/components/adminView/modals/CreateQuestion.js
@@ -13,16 +13,18 @@ const CreateQuestion = props => {
         <div className="modal">
             <form action={link} method="post" className="question-box">
                 <input name="questionId" type="hidden" value={props.questionId} />
-                <input name="points" className="input standard-spacing" type="number" placeholder="Enter Points" />
-                <textarea name="question" className="input question-input standard-spacing" placeholder="Enter Question"></textarea>
-                <Container>
-                    <input name="option1" className="input option-input standard-spacing" type="text" placeholder="Enter Option 1"/>
-                    <input name="option2" className="input option-input standard-spacing" type="text" placeholder="Enter Option 2"/>
-                    <input name="option3" className="input option-input standard-spacing" type="text" placeholder="Enter Option 3"/>
-                    <input name="option4" className="input option-input standard-spacing" type="text" placeholder="Enter Option 4"/>
+                <textarea name="question" className="input question-input standard-spacing" placeholder="Question"></textarea>
+                <Container className="margin-spacing">
+                    <input name="option1" className="input option-input standard-spacing" type="text" placeholder="Option 1"/>
+                    <input name="option2" className="input option-input standard-spacing" type="text" placeholder="Option 2"/>
+                    <input name="option3" className="input option-input standard-spacing" type="text" placeholder="Option 3"/>
+                    <input name="option4" className="input option-input standard-spacing" type="text" placeholder="Option 4"/>
                 </Container>
-                <input name="correctOption" className="input standard-spacing" type="number" placeholder="Correct Option Number" />
-                <Container>
+                <Container className="margin-spacing">
+                    <input name="correctOption" className="input big-input standard-spacing" type="number" placeholder="Correct Option Number" />
+                    <input name="points" className="input small-input standard-spacing" type="number" placeholder="Points" />
+                </Container>
+                <Container className="margin-spacing">
                     <button onClick={props.onClose} className="input submit-input standard-spacing green white-text">Close</button>
                     <input type="submit" className="input submit-input standard-spacing dark-green white-text" value="Save Question" />
                 </Container>
