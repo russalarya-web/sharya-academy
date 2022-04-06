f454a96c5934451bfd4e168bee9c1e04efc64387
Russal Arya <russalarya@gmail.com>
2022-04-06 14:42:11 +0530

    Updated Access Denied Page

--- client/src/routes/UserView.tsx
+++ client/src/routes/UserView.tsx
@@ -167,7 +167,17 @@ function UserView() {
     } else {
         return (
             <Page>
-                <h1>You do not have access to this page. <Link href="/login">Sign in</Link> now.</h1>
+                <h1>You do not have access to this page.</h1>
+                <button 
+                    className="green standard-spacing white-text"
+                    onClick={() =>{window.location.href = "/login"}}>
+                    Sign in
+                </button>
+                <button 
+                    className="dark-green standard-spacing white-text"
+                    onClick={() =>{window.location.href = "/sign-up"}}>
+                    Create an account
+                </button>
             </Page>
         )
     }
